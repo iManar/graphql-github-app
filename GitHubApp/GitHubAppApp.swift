13961ffe9da4794064cd3a40b159036683539ed6
manmagdy <manar.magdy@booking.com>
2022-12-15 01:49:18 +0200

    Initial Commit

---
+++ GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by Manar Magdy on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
